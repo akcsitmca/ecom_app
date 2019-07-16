f4cabd195de7ea1b0d8b8a147848bf086bb5fce3
akcsitmca <akcsitmca2017@gmail.com>
2019-07-16 19:49:14 +0530

    login activity complete

--- app/src/main/java/com/example/ecom_app/MainActivity.java
+++ app/src/main/java/com/example/ecom_app/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.ecom_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_login, btn_signup;
+    private int INTERNET_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +32,75 @@ public class MainActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED){
+
+
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    requestInternetPermission();
+
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
             }
         });
 
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
+
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED){
+
+                    Toast.makeText(MainActivity.this, "have Permission", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    requestInternetPermission();
+
+                    Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+
+//                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+//                startActivity(intent);
             }
         });
+    }
+
+    private void requestInternetPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("To create or access your account internet access is required")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.INTERNET}, INTERNET_PERMISSION_CODE);
+
+                        }
+                    })
+                    .create().show();
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.INTERNET}, INTERNET_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == INTERNET_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ecom_app/RegisterActivity.java
+++ app/src/main/java/com/example/ecom_app/RegisterActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -50,38 +51,73 @@ public class RegisterActivity extends AppCompatActivity {
     private void createAccount() {
         String name = InputName.getText().toString();
         String phone = InputPhoneNumber.getText().toString();
-        String passsword = InputPassword.getText().toString();
+        String password = InputPassword.getText().toString();
 
-        if(TextUtils.isEmpty(name)){
-            Toast.makeText(RegisterActivity.this, "Please write your name", Toast.LENGTH_SHORT);
-        }
-        else if(TextUtils.isEmpty(phone)){
-            Toast.makeText(RegisterActivity.this, "Please write phone number", Toast.LENGTH_SHORT);
-        }
-        else if(TextUtils.isEmpty(passsword)){
-            Toast.makeText(RegisterActivity.this, "Please write password", Toast.LENGTH_SHORT);
-        }
-        else{
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(RegisterActivity.this, "Please write your name", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(RegisterActivity.this, "Please write phone number", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "Please write password", Toast.LENGTH_SHORT).show();
+        } else {
             loadingBar.setTitle("Create Account");
-            loadingBar.setMessage("Please Wait, we are checking credentials");;
+            loadingBar.setMessage("Please Wait, we are checking credentials");
             loadingBar.setCancelable(false);
             loadingBar.show();
-            
-            validatephonenumber(name, phone, passsword);
+
+//            final DatabaseReference RootRef;
+//            RootRef = FirebaseDatabase.getInstance().getReference();
+//
+//            Hashtable<String, Object> userdateMap = new Hashtable<>();
+//            Toast.makeText(RegisterActivity.this,"userdatemap created",Toast.LENGTH_SHORT).show();
+//            userdateMap.put("phone", phone);
+//            Toast.makeText(RegisterActivity.this,"phone updated",Toast.LENGTH_SHORT).show();
+//            userdateMap.put("password", password);
+//            Toast.makeText(RegisterActivity.this,"password updated",Toast.LENGTH_SHORT).show();
+//            userdateMap.put("name", name);
+//            Toast.makeText(RegisterActivity.this,"userdatemap updated",Toast.LENGTH_SHORT).show();
+//
+//            RootRef.child("phone").child(phone).updateChildren(userdateMap)
+//                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<Void> task) {
+//                            if (task.isSuccessful()){
+//                                Toast.makeText(RegisterActivity.this, "Congratulation, your account is created", Toast.LENGTH_SHORT).show();
+//                                loadingBar.dismiss();
+//
+//                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+//                                startActivity(intent);
+//
+//                                finish();
+//                            }
+//                            else {
+//                                loadingBar.dismiss();
+//                                Toast.makeText(RegisterActivity.this, "Network Issue, Please try again", Toast.LENGTH_SHORT).show();
+//
+//                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+//                                startActivity(intent);
+//
+//                                finish();
+//                            }
+//                        }
+//                    });
+
+
+            validatephonenumber(name, phone, password);
         }
     }
 
-    private void validatephonenumber(final String name, final String phone, final String passsword) {
+    private void validatephonenumber(final String name, final String phone, final String password) {
         final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 if(!(dataSnapshot.child("Users").child(phone).exists())){
                     HashMap<String, Object> userdateMap = new HashMap<>();
                     userdateMap.put("phone", phone);
-                    userdateMap.put("password", passsword);
+                    userdateMap.put("password", password);
                     userdateMap.put("name", name);
 
                     RootRef.child("Users").child(phone).updateChildren(userdateMap)
@@ -89,7 +125,7 @@ public class RegisterActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()){
-                                        Toast.makeText(RegisterActivity.this, "Congratulation, your account is created", Toast.LENGTH_SHORT);
+                                        Toast.makeText(RegisterActivity.this, "Congratulation, your account is created", Toast.LENGTH_SHORT).show();
                                         loadingBar.dismiss();
 
                                         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
@@ -99,7 +135,7 @@ public class RegisterActivity extends AppCompatActivity {
                                     }
                                     else {
                                         loadingBar.dismiss();
-                                        Toast.makeText(RegisterActivity.this, "Network Issue, Please try again", Toast.LENGTH_SHORT);
+                                        Toast.makeText(RegisterActivity.this, "Network Issue, Please try again", Toast.LENGTH_SHORT).show();
 
                                         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                                         startActivity(intent);
@@ -110,9 +146,9 @@ public class RegisterActivity extends AppCompatActivity {
                             });
                 }
                 else {
-                    Toast.makeText(RegisterActivity.this, "Phone Number" + phone + "already exist", Toast.LENGTH_SHORT);
+                    Toast.makeText(RegisterActivity.this, "Phone Number " + phone + " already exist", Toast.LENGTH_SHORT).show();
                     loadingBar.dismiss();
-                    Toast.makeText(RegisterActivity.this, "Please try with another phone number", Toast.LENGTH_SHORT);
+                    Toast.makeText(RegisterActivity.this, "Please try with another phone number", Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                     startActivity(intent);
